1be34d54ed2e5514099924b6fd51e69dc67903ec
Jacobokia <40197720+Jacobokia@users.noreply.github.com>
2019-07-31 13:54:27 +0300

    Add files via upload

---
+++ first_exercise.py
@@ -0,0 +1,35 @@
+#function without a parameter
+
+def my_function():
+    print("Hello")
+
+my_function()
+
+# Function with a parameter
+
+def first_name(fname):
+    print(fname + "Okia")
+
+first_name("Jacob ")
+
+# Lists in Python
+fruit_list = ["oranges", "Mangoes", "Pawpaws"]
+print(fruit_list)
+
+# How to add an item into the fruit_list
+
+fruit_list[2]= "Passion fruit"
+print(fruit_list)
+
+# how to loop through a list.
+
+for item in fruit_list:
+    print(item)
+
+#add an item to the list.
+fruit_list.append("Kola")
+print(fruit_list)
+
+
+# Set
+my_list = {"Okia","Jacob", "general"}
